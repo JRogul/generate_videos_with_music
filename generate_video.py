570d4f47718df85695e7a08c35e148fc3e952b7b
jakub <jakubrogulski01@gmail.com>
2023-07-04 14:11:15 +0200

    generates video

---
+++ generate_video.py
@@ -0,0 +1,14 @@
+import torch
+from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers.utils import export_to_video
+import cv2
+
+def create_video(prompt, output_path='outputs//vvaavideo.mp4'):
+        
+    pipe = DiffusionPipeline.from_pretrained("cerspense/zeroscope_v2_576w", torch_dtype=torch.float16)
+    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    pipe.enable_model_cpu_offload()
+
+    video_frames = pipe(prompt, num_inference_steps=40, height=320, width=576, num_frames=10).frames
+    video_path = export_to_video(video_frames, output_path)
+    return video_path
