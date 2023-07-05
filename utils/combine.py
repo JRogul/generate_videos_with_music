b1843c512abba67b5066fce089382b0fec8440ad
jakub <jakubrogulski01@gmail.com>
2023-07-05 13:20:23 +0200

    combines video and audio

---
+++ utils/combine.py
@@ -0,0 +1,8 @@
+from moviepy.editor import VideoFileClip, AudioFileClip
+
+def combine(video_path = 'outputs//video.mp4', audio_path = 'outputs//audio.wav', output_path = 'outputs//final_video.mp4'):
+    video = VideoFileClip(video_path)
+    audio = AudioFileClip(audio_path)
+    video = video.set_audio(audio)
+    video.write_videofile(output_path)
+    return output_path
